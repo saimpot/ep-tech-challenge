a699b8ae9fe40a6963c387c6c486a790620aadbe
saimpot <saimpot@gmail.com>
2024-12-17 02:08:49 +0200

    [FEATURE-1] Add missing relationships to `Client` and `User` models

--- app/Client.php
+++ app/Client.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
@@ -19,17 +21,22 @@ class Client extends Model
         'url',
     ];
 
-    public function bookings()
+    public function bookings(): HasMany
     {
         return $this->hasMany(Booking::class);
     }
 
-    public function getBookingsCountAttribute()
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getBookingsCountAttribute(): int
     {
         return $this->bookings->count();
     }
 
-    public function getUrlAttribute()
+    public function getUrlAttribute(): string
     {
         return "/clients/" . $this->id;
     }
